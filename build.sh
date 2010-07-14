856e771790bdb13f073cc0bf9214fd7bb3b58e7f
bellbind <bellbind@gmail.com>
2010-07-14 13:26:19 +0900

    [support] windows dll generation

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+if uname | grep "MINGW32" ; then
+  EXT=pyd
+elif uname | grep "CYGWIN" ; then
+  EXT=dll
+else
+  EXT=so
+fi
+
 # apt-get install libxqilla-dev libxerces-c2-dev
 g++ -Wall -fPIC -c xqilla.cpp -o xqilla.o
 gcc -Wall -fPIC `python-config --cflags` -c xquery.c -o xquery.o
 g++ -Wall `python-config --ldflags` -lxerces-c -lxqilla -shared \
-    -o simplexquery.so xqilla.o xquery.o 
+    -o simplexquery.$EXT xqilla.o xquery.o 
 rm xqilla.o xquery.o
