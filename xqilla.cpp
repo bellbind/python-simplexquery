4189933ecfd59c8a098dc2fc7d4f146aac74fffb
bellbind <bellbind@gmail.com>
2010-07-16 14:53:18 +0900

    [modify] indent style to python-new at http://wiki.python.org/moin/EmacsEditor

--- xqilla.cpp
+++ xqilla.cpp
@@ -7,157 +7,165 @@
 #include <xercesc/util/XMLURL.hpp>
 
 namespace {
-  class CallbackURIResolver : public URIResolver 
-  {
-  private:
-    char * (* callback)(void *, const char *);
-    void * callback_arg;
+    class CallbackURIResolver : public URIResolver 
+    {
+    private:
+        char * (* callback)(void *, const char *);
+        void * callback_arg;
     
-  public:
-    CallbackURIResolver(char * (* _callback)(void *, const char *), 
-                        void * _callback_arg) :
-      callback(_callback), callback_arg(_callback_arg) {}
-    virtual ~CallbackURIResolver() {}
-    
-    
-    virtual bool 
-    resolveDocument(Sequence & result, const XMLCh * uri, 
-                    DynamicContext * context, 
-                    const QueryPathNode * projection) {
-      const XMLCh * id = uri;
-      XERCES_CPP_NAMESPACE::XMLURL url(context->getMemoryManager());
-      if(url.setURL(context->getBaseURI(), uri, url)) {
-        id = context->getMemoryManager()->getPooledString(url.getURLText());
-      }
-      std::string stduri(UTF8(url.getURLText()));
-      char * cxml = callback(callback_arg, stduri.c_str());
-      if (!cxml) return false;
-      std::string xml(cxml);
-      free(cxml);
-      
-      XERCES_CPP_NAMESPACE::MemBufInputSource 
-      is(reinterpret_cast<const XMLByte *>(xml.c_str()), xml.length(), id);
-      Node::Ptr doc = context->parseDocument(is);
-      result.addItem(doc);
-      return true;
-    }
-    
-    virtual bool 
-    resolveCollection(Sequence & result, const XMLCh * uri, 
-                      DynamicContext * context, 
-                      const QueryPathNode * projection) {
-      return false;
-    }
-    
-    virtual bool 
-    resolveDefaultCollection(Sequence & result, 
-                             DynamicContext * context, 
-                             const QueryPathNode * projection) {
-      return false;
-    }
-    
-    virtual bool putDocument(const Node::Ptr & document, const XMLCh * uri, 
-                             DynamicContext * context) {
-      return false;
-    }
-  };
-  
-  class Executor {
-    const char * xquery;
-    const char * context_xml;
-    char * (* resolver)(void *, const char *);
-    void * resolver_arg;
-    XQilla xqilla;
-    AutoDelete<XQQuery> query;
-    AutoDelete<DynamicContext> context;
-    
-  public:
-    Executor(const char * _xquery, const char * _context_xml,
-             char * (* _resolver)(void *, const char *), 
-             void * _resolver_arg) :
-      xquery(_xquery), context_xml(_context_xml), 
-      resolver(_resolver), resolver_arg(_resolver_arg),
-      xqilla(), query(xqilla.parse(X(xquery))), 
-      context(query->createDynamicContext()) {
-      setup();
-    }
-    
-    char * execute() {
-      Result result = query->execute(context);
-      
-      Item::Ptr item;
-      while (item = result->next(context)) {
-        std::string stdval(UTF8(item->asString(context)));
-        char * buf = reinterpret_cast<char *>(malloc(stdval.length() + 1));
-        if (buf == NULL) return NULL;
-        strcpy(buf, stdval.c_str());
-        return buf;
-      }
-      return NULL;
-    }
-    
-    int execute_all(void (* callback)(void *, const char *), 
-                    void * callback_arg) {
-      Result result = query->execute(context);
-      
-      Item::Ptr item;
-      while (item = result->next(context)) {
-        std::string stdval(UTF8(item->asString(context)));
-        callback(callback_arg, stdval.c_str());
-      }
-      return 1;
-    }
-    
-  private:
-    void setup() {
-      if (context_xml) {
-        std::string xml(context_xml);
-        XERCES_CPP_NAMESPACE::MemBufInputSource 
-        is(reinterpret_cast<const XMLByte *>(xml.c_str()), 
-	   xml.length(), ".");
-        Node::Ptr doc = context->parseDocument(is);
-        Sequence seq(doc);
-        if (!seq.isEmpty() && seq.first()->isNode()) {
-          context->setContextItem(seq.first());
-          context->setContextPosition(1);
-          context->setContextSize(1);
+    public:
+        CallbackURIResolver(
+            char * (* _callback)(void *, const char *), 
+            void * _callback_arg) :
+            callback(_callback), callback_arg(_callback_arg) {}
+        virtual ~CallbackURIResolver() {}
+        
+        
+        virtual bool 
+        resolveDocument(Sequence & result, const XMLCh * uri, 
+                        DynamicContext * context, 
+                        const QueryPathNode * projection) {
+            const XMLCh * id = uri;
+            XERCES_CPP_NAMESPACE::XMLURL url(context->getMemoryManager());
+            if (url.setURL(context->getBaseURI(), uri, url)) {
+                id = context->getMemoryManager()->getPooledString(
+                    url.getURLText());
+            }
+            std::string stduri(UTF8(url.getURLText()));
+            char * cxml = callback(callback_arg, stduri.c_str());
+            if (!cxml) return false;
+            std::string xml(cxml);
+            free(cxml);
+            
+            XERCES_CPP_NAMESPACE::MemBufInputSource 
+            is(reinterpret_cast<const XMLByte *>(xml.c_str()),
+               xml.length(), id);
+            Node::Ptr doc = context->parseDocument(is);
+            result.addItem(doc);
+            return true;
         }
-      }
-      if (resolver) {
-        URIResolver * uri_resolver = new CallbackURIResolver(resolver, 
-                                                             resolver_arg);
-        context->registerURIResolver(uri_resolver, true);
-      }
-    }
-  };
+    
+        virtual bool 
+        resolveCollection(Sequence & result, const XMLCh * uri, 
+                          DynamicContext * context, 
+                          const QueryPathNode * projection) {
+            return false;
+        }
+    
+        virtual bool 
+        resolveDefaultCollection(Sequence & result, 
+                                 DynamicContext * context, 
+                                 const QueryPathNode * projection) {
+            return false;
+        }
+        
+        virtual bool putDocument(const Node::Ptr & document, 
+                                 const XMLCh * uri, 
+                                 DynamicContext * context) {
+            return false;
+        }
+    };
+  
+    class Executor {
+        const char * xquery;
+        const char * context_xml;
+        char * (* resolver)(void *, const char *);
+        void * resolver_arg;
+        XQilla xqilla;
+        AutoDelete<XQQuery> query;
+        AutoDelete<DynamicContext> context;
+        
+    public:
+        Executor(const char * _xquery, const char * _context_xml,
+                 char * (* _resolver)(void *, const char *), 
+                 void * _resolver_arg) :
+            xquery(_xquery), context_xml(_context_xml), 
+            resolver(_resolver), resolver_arg(_resolver_arg),
+            xqilla(), query(xqilla.parse(X(xquery))), 
+            context(query->createDynamicContext()) {
+            setup();
+        }
+        
+        char * execute() {
+            Result result = query->execute(context);
+            
+            Item::Ptr item;
+            while (item = result->next(context)) {
+                std::string stdval(UTF8(item->asString(context)));
+                char * buf = reinterpret_cast<char *>(
+                    malloc(stdval.length() + 1));
+                if (buf == NULL) return NULL;
+                strcpy(buf, stdval.c_str());
+                return buf;
+            }
+            return NULL;
+        }
+    
+        int execute_all(void (* callback)(void *, const char *), 
+                        void * callback_arg) {
+            Result result = query->execute(context);
+            
+            Item::Ptr item;
+            while (item = result->next(context)) {
+                std::string stdval(UTF8(item->asString(context)));
+                callback(callback_arg, stdval.c_str());
+            }
+            return 1;
+        }
+    
+    private:
+        void setup() {
+            if (context_xml) {
+                std::string xml(context_xml);
+                XERCES_CPP_NAMESPACE::MemBufInputSource 
+                is(reinterpret_cast<const XMLByte *>(xml.c_str()), 
+                   xml.length(), ".");
+                Node::Ptr doc = context->parseDocument(is);
+                Sequence seq(doc);
+                if (!seq.isEmpty() && seq.first()->isNode()) {
+                    context->setContextItem(seq.first());
+                    context->setContextPosition(1);
+                    context->setContextSize(1);
+                }
+            }
+            if (resolver) {
+                URIResolver * uri_resolver = new CallbackURIResolver(
+                    resolver, resolver_arg);
+                context->registerURIResolver(uri_resolver, true);
+            }
+        }
+    };
 }
 
 extern "C" { 
-  extern char *
-  execute(const char * xquery, const char * context_xml,
-          char * (* resolver)(void *, const char *), void * resolver_arg)
-  {
+
+extern char *
+execute(const char * xquery, const char * context_xml,
+        char * (* resolver)(void *, const char *), void * resolver_arg)
+{
     try {
-      Executor executor(xquery, context_xml, resolver, resolver_arg);
-      return executor.execute();
+        Executor executor(xquery, context_xml, resolver, resolver_arg);
+        return executor.execute();
     } catch (XQException ex) {
-      ;
+        ;
     } catch (...) {}
     return NULL;
-  }
+}
 
-  extern int
-  execute_all(const char * xquery, const char * context_xml, 
-              char * (* resolver)(void *, const char *), 
-              void * resolver_arg,
-              void (* callback)(void *, const char *), void * callback_arg)
-  {
+extern int
+execute_all(const char * xquery, const char * context_xml, 
+            char * (* resolver)(void *, const char *), 
+            void * resolver_arg,
+            void (* callback)(void *, const char *), void * callback_arg)
+{
     try {
-      Executor executor(xquery, context_xml, resolver, resolver_arg);
-      return executor.execute_all(callback, callback_arg);
+        Executor executor(xquery, context_xml, resolver, resolver_arg);
+        return executor.execute_all(callback, callback_arg);
     } catch (XQException ex) {
-      ;
+        ;
     } catch (...) {}
     return 0;
-  }
 }
+
+}
+
--- xquery.c
+++ xquery.c
@@ -4,135 +4,137 @@
 
 typedef char * (* resolver_t)(void *, const char *);
 
-extern char * execute(const char *, const char *,
-                      resolver_t, void *);
-extern int execute_all(const char *, const char *, 
-                       resolver_t, void *,
-                       void (void *, const char *), void *);
+extern char * execute(
+    const char *, const char *, resolver_t, void *);
+extern int execute_all(
+    const char *, const char *, resolver_t, void *,
+    void (void *, const char *), void *);
 
 static void
 append_pylist(void * pylist, const char * buf) 
 {
-  PyObject * list = pylist;
-  PyObject * ret = PyUnicode_DecodeUTF8(buf, strlen(buf), NULL);
-  PyList_Append(list, ret);
-  Py_XDECREF(ret);
+    PyObject * list = pylist;
+    PyObject * ret = PyUnicode_DecodeUTF8(buf, strlen(buf), NULL);
+    PyList_Append(list, ret);
+    Py_XDECREF(ret);
 }
 
 static char *
 call_resolver(void * resolver, const char * uri) 
 {
-  PyObject * callable = resolver;
-  PyObject * uniuri = PyUnicode_DecodeUTF8(uri, strlen(uri), NULL);
-  PyObject * args = PyTuple_Pack(1, uniuri);
-  PyObject * strret = PyObject_CallObject(callable, args);
-  PyObject * rets = PyTuple_Pack(1, strret);
-  char * cpret = NULL;
-  int ok = PyArg_ParseTuple(rets, "es", "utf-8", &cpret);
-  char * retstr = NULL;
-  if (ok) {
-    size_t len = strlen(cpret);
-    retstr = malloc(len + 1);
-    if (retstr != NULL) strcpy(retstr, cpret);
-  }
-  Py_XDECREF(rets);
-  Py_XDECREF(strret);
-  Py_XDECREF(args);
-  Py_XDECREF(uniuri);
-  return retstr;
+    PyObject * callable = resolver;
+    PyObject * uniuri = PyUnicode_DecodeUTF8(uri, strlen(uri), NULL);
+    PyObject * args = PyTuple_Pack(1, uniuri);
+    PyObject * strret = PyObject_CallObject(callable, args);
+    PyObject * rets = PyTuple_Pack(1, strret);
+    char * cpret = NULL;
+    int ok = PyArg_ParseTuple(rets, "es", "utf-8", &cpret);
+    char * retstr = NULL;
+    if (ok) {
+        size_t len = strlen(cpret);
+        retstr = malloc(len + 1);
+        if (retstr != NULL) strcpy(retstr, cpret);
+    }
+    Py_XDECREF(rets);
+    Py_XDECREF(strret);
+    Py_XDECREF(args);
+    Py_XDECREF(uniuri);
+    return retstr;
 }
 
 static int
-execute_parse_params(PyObject * args, PyObject * kwargs,
-                     char ** pxquery, char ** pcontext_xml, 
-                     PyObject ** presolver, resolver_t * presolver_func)
+execute_parse_params(
+    PyObject * args, PyObject * kwargs, 
+    char ** pxquery, char ** pcontext_xml,
+    PyObject ** presolver, resolver_t * presolver_func)
 {
-  static char * kwlist[] = {"xquery_code", "context_xml", "resolver", NULL};
-  int ok = PyArg_ParseTupleAndKeywords(args, kwargs, "es|esO", kwlist, 
-                                       "utf-8", pxquery, 
-                                       "utf-8", pcontext_xml,
-                                       presolver);
-  if (!ok) return 0;
-  if (*presolver) {
-    if (!PyCallable_Check(*presolver)) return 0;
-    *presolver_func = &call_resolver;
-  }
-  return 1;
+    static char * kwlist[] = {"xquery_code", "context_xml", "resolver", NULL};
+    int ok = PyArg_ParseTupleAndKeywords(
+        args, kwargs, "es|esO", kwlist, 
+        "utf-8", pxquery, 
+        "utf-8", pcontext_xml,
+        presolver);
+    if (!ok) return 0;
+    if (*presolver) {
+        if (!PyCallable_Check(*presolver)) return 0;
+        *presolver_func = &call_resolver;
+    }
+    return 1;
 }
 
 static PyObject *
 xquery_execute(PyObject * self, PyObject * args, PyObject * kwargs)
 {
-  char * xquery = NULL;
-  char * context_xml = NULL;
-  PyObject * resolver = NULL;
-  resolver_t resolver_func = NULL;
-  int ok = execute_parse_params(args, kwargs,
-                                &xquery, &context_xml, 
-                                &resolver, &resolver_func);
-  if (ok) {
-    char * buf = execute(xquery, context_xml, 
-                         resolver_func, resolver);
-    if (buf) {
-      PyObject * ret = PyUnicode_DecodeUTF8(buf, strlen(buf), NULL);
-      free(buf);
-      return ret;
+    char * xquery = NULL;
+    char * context_xml = NULL;
+    PyObject * resolver = NULL;
+    resolver_t resolver_func = NULL;
+    int ok = execute_parse_params(
+        args, kwargs,
+        &xquery, &context_xml, &resolver, &resolver_func);
+    if (ok) {
+        char * buf = execute(
+            xquery, context_xml, resolver_func, resolver);
+        if (buf) {
+            PyObject * ret = PyUnicode_DecodeUTF8(buf, strlen(buf), NULL);
+            free(buf);
+            return ret;
+        }
     }
-  }
-  Py_RETURN_NONE;
+    Py_RETURN_NONE;
 }
 
 
 static PyObject *
 xquery_execute_all(PyObject * self, PyObject * args, PyObject* kwargs)
 {
-  char * xquery = NULL;
-  char * context_xml = NULL;
-  PyObject * resolver = NULL;
-  resolver_t resolver_func = NULL;
-  int ok = execute_parse_params(args, kwargs,
-                                &xquery, &context_xml, 
-                                &resolver, &resolver_func);
-  if (ok) {
-    PyObject * pylist = PyList_New(0);
-    int ret = execute_all(xquery, context_xml, 
-                          resolver_func, resolver,
-                          &append_pylist, pylist);
-    if (ret) return pylist;
-    Py_XDECREF(pylist);
-    PyErr_SetString(PyExc_ValueError, "invalid args");
-  }
-  return NULL;  
+    char * xquery = NULL;
+    char * context_xml = NULL;
+    PyObject * resolver = NULL;
+    resolver_t resolver_func = NULL;
+    int ok = execute_parse_params(
+        args, kwargs,
+        &xquery, &context_xml, &resolver, &resolver_func);
+    if (ok) {
+        PyObject * pylist = PyList_New(0);
+        int ret = execute_all(
+            xquery, context_xml, resolver_func, resolver,
+            &append_pylist, pylist);
+        if (ret) return pylist;
+        Py_XDECREF(pylist);
+        PyErr_SetString(PyExc_ValueError, "invalid args");
+    }
+    return NULL;  
 }
 
 static PyMethodDef  XQueryMethods[] = {
-  {"execute", (PyCFunction) xquery_execute, 
-   METH_VARARGS | METH_KEYWORDS, 
-   "execute(xquery_code[, context_xml][, resolver]) -> unicode"
-  },
-  {"execute_all", (PyCFunction) xquery_execute_all, 
-   METH_VARARGS | METH_KEYWORDS, 
-   "execute_all(xquery_code[, context_xml][, resolver]) -> [unicode]"
-  },
-  {NULL, NULL, 0, NULL}
+    {"execute", (PyCFunction) xquery_execute, 
+     METH_VARARGS | METH_KEYWORDS, 
+     "execute(xquery_code[, context_xml][, resolver]) -> unicode"
+    },
+    {"execute_all", (PyCFunction) xquery_execute_all, 
+     METH_VARARGS | METH_KEYWORDS, 
+     "execute_all(xquery_code[, context_xml][, resolver]) -> [unicode]"
+    },
+    {NULL, NULL, 0, NULL}
 };
 
 #if PY_MAJOR_VERSION >= 3
 
 static struct PyModuleDef XQueryModule = {
-  PyModuleDef_HEAD_INIT,
-  "simplexquery",
-  NULL,
-  -1,
-  XQueryMethods
+    PyModuleDef_HEAD_INIT,
+    "simplexquery",
+    NULL,
+    -1,
+    XQueryMethods
 };
 
 PyMODINIT_FUNC
 PyInit_simplexquery(void)
 {
-  PyObject * m = PyModule_Create(&XQueryModule);
-  if (m == NULL) return NULL;
-  return m;
+    PyObject * m = PyModule_Create(&XQueryModule);
+    if (m == NULL) return NULL;
+    return m;
 }
 
 #else
@@ -140,8 +142,8 @@ PyInit_simplexquery(void)
 PyMODINIT_FUNC
 initsimplexquery(void)
 {
-  PyObject * m = Py_InitModule("simplexquery", XQueryMethods);
-  if (m == NULL) return;
+    PyObject * m = Py_InitModule("simplexquery", XQueryMethods);
+    if (m == NULL) return;
 }
 
 #endif
