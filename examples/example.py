8813a17d8357123ee6578f2388b1abf71453d6f1
bellbind <bellbind@gmail.com>
2010-09-15 15:56:32 +0900

    [merge] example3.py to example.py

--- examples/example.py
+++ examples/example.py
@@ -1,20 +1,21 @@
+import traceback
 import simplexquery as sxq
 
+def prompt(title):
+    print("\n" + title)
+    pass
 
-print("""
-['execute' xquery code]\
-""")
+
+prompt("['execute' xquery code]")
 print(sxq.execute("""<user>{"Taro"}</user>"""))
 
-print("""
-['execute' xquery code with context xml]\
-""")
+
+prompt("['execute' xquery code with context xml]")
 print(sxq.execute("<html><body>{string(/author/name)}</body></html>",
                   "<author><name>Taro</name></author>"))
 
-print("""
-['execute' xquery code with uri -> xml resolver callback]\
-""")
+
+prompt("['execute' xquery code with uri -> xml resolver callback]")
 print(sxq.execute('<body>{doc(/root/@href)/name}</body>', 
                   "<root href='/bar.xml'/>", 
                   lambda uri: "<name>%s</name>" % uri))
@@ -26,70 +27,40 @@ class Resolver(object):
     pass
 print(sxq.execute('doc("foo.xml")/name', resolver=Resolver()))
 
-print("""
-['execute' returns None if fails by anyway]\
-""")
+
+prompt("['execute' returns None if fails by anyway]")
 print(sxq.execute("<user>")) 
 print(sxq.execute("<user>{string(/name)}</user>", "<name>Taro<name>")) 
 
 
-
-print("""
-['execute_all' returns a list of multiple results]\
-""")
+prompt("['execute_all' returns a list of multiple results]")
 print(repr(sxq.execute_all("/user/name",
                            "<user><name>Taro</name><name>Jiro</name></user>")))
 
-print("""
-['execute_all' raise ValueError if xquery execution is failed]\
-""")
-try:
-    sxq.execute_all("/user'")
-    pass
-except ValueError, ex:
-    print(ex)
-    pass
-try:
-    sxq.execute_all('doc("foo.xml")/name', resolver=lambda uri : "non xml")
-    pass
-except ValueError, ex:
-    print(ex)
-    pass
 
-print("""
-['execute_all' raise TypeError if arg has invalid type]\
-""")
-try:
-    sxq.execute_all(0)
-    pass
-except TypeError, ex:
-    print(ex)
-    pass
-try:
-    sxq.execute_all("", "", 0)
-    pass
-except TypeError, ex:
-    print(ex)
-    pass
+prompt("['execute_all' raise ValueError if xquery execution is failed]")
+try: sxq.execute_all("/user'")
+except ValueError: traceback.print_exc()
 
-try:
-    sxq.execute_all('doc("foo.xml")/name', resolver=lambda : "abc")
-    pass
-except TypeError, ex:
-    print(ex)
-    pass
-try:
-    sxq.execute_all('doc("foo.xml")/name', resolver=lambda a : None)
-    pass
-except TypeError, ex:
-    print(ex)
-    pass
+try: sxq.execute_all('doc("foo.xml")/name', resolver=lambda uri : "non xml")
+except ValueError: traceback.print_exc()
 
-print("""
-['versions' returns version tuple]\
-""")
+
+prompt("['execute_all' raise TypeError if arg has invalid type]")
+try: sxq.execute_all(0)
+except TypeError: traceback.print_exc()
+
+try: sxq.execute_all("", "", 0)
+except TypeError: traceback.print_exc()
+
+try: sxq.execute_all('doc("foo.xml")/name', resolver=lambda : "abc")
+except TypeError: traceback.print_exc()
+
+try: sxq.execute_all('doc("foo.xml")/name', resolver=lambda a : None)
+except TypeError: traceback.print_exc()
+
+
+prompt("['versions' returns version tuple]")
 print(sxq.versions())
 
-print("""
-[end]\
-""")
+prompt("[end]")
